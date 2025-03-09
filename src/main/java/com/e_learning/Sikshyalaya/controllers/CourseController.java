594fb665cc4bfa1704bd593a76b1f5410124560c
raghab007 <pokhrelraghab3@gmail.com>
2025-03-09 20:19:15 +0545

    Get course categories

--- src/main/java/com/e_learning/Sikshyalaya/controllers/CourseController.java
+++ src/main/java/com/e_learning/Sikshyalaya/controllers/CourseController.java
@@ -1,7 +1,9 @@
 package com.e_learning.Sikshyalaya.controllers;
 
 import com.e_learning.Sikshyalaya.entities.Course;
+import com.e_learning.Sikshyalaya.entities.CourseCategory;
 import com.e_learning.Sikshyalaya.entities.Section;
+import com.e_learning.Sikshyalaya.repositories.CategoryRepository;
 import com.e_learning.Sikshyalaya.service.CourseService;
 import com.e_learning.Sikshyalaya.service.SectionService;
 import org.springframework.http.HttpStatus;
@@ -19,9 +21,12 @@ public class CourseController {
 
    private  final SectionService sectionService;
 
-    public  CourseController(CourseService courseService, SectionService sectionService){
+   private  final CategoryRepository categoryRepository;
+
+    public  CourseController(CourseService courseService, SectionService sectionService, CategoryRepository categoryRepository){
         this.courseService = courseService;
         this.sectionService = sectionService;
+        this.categoryRepository = categoryRepository;
     }
     @GetMapping("/courses")
     public List<Course> getAllCourses(){
@@ -39,5 +44,11 @@ public class CourseController {
         List<Section> sections = sectionService.findSectionsByCourse(course.getCourseID());
         course.setSections(sections);
         return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    @GetMapping("/course/course_category")
+    public List<CourseCategory> getAllCourseCategories(){
+        List<CourseCategory> all = categoryRepository.findAll();
+        return  all;
     }
 }
